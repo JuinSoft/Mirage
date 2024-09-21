5f493803ce53d7efb113083f066a3d0899c109c7
Sonali Thakur <sonalithakur1199@gmail.com>
2024-09-22 01:13:34 +0800

    nounsDAO UI changes

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,6 +13,18 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
+const silkscreenRegular = localFont({
+  src: "./fonts/Silkscreen-Regular.ttf",
+  variable: "--font-silkscreen-regular",
+  weight: "normal",
+});
+
+const silkscreenBold = localFont({
+  src: "/fonts/Silkscreen-Bold.ttf",
+  variable: "--font-silkscreen-bold",
+  weight: "bold",
+});
+
 export const metadata = {
   title: "Mirage",
   description: "Decentralized Transactional Email",
@@ -22,7 +34,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${silkscreenRegular.variable} ${silkscreenBold.variable} antialiased`}
       >
         <DynamicAuth>
           {children}
--- src/app/page.js
+++ src/app/page.js
@@ -8,36 +8,28 @@ import { useRouter } from 'next/navigation';
 
 const sidebarItems = [
   {
-    icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M22 6C22 4.9 21.1 4 20 4H4C2.9 4 2 4.9 2 6V18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6ZM20 6L12 11L4 6H20ZM20 18H4V8L12 13L20 8V18Z" fill="currentColor" />
-    </svg>, label: 'Inbox'
+    icon: <img src="/assets_png/traits-svg/3-heads/head-mailbox.svg" width="32" height="32" alt="Inbox" />, 
+    label: 'Inbox'
   },
   {
-    icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" fill="currentColor" />
-    </svg>, label: 'Starred'
+    icon: <img src="/assets_png/traits-svg/3-heads/head-star-sparkles.svg" width="32" height="32" alt="Starred" />, 
+    label: 'Starred'
   },
   {
-    icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M2.01 21L23 12L2.01 3L2 10L17 12L2 14L2.01 21Z" fill="currentColor" />
-    </svg>, label: 'Sent'
+    icon: <img src="/assets_png/traits-svg/3-heads/head-plane.svg" width="32" height="32" alt="Starred" />, 
+   label: 'Sent'
   },
   {
-    icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M20 6H12L10 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V8C22 6.9 21.1 6 20 6ZM20 18H4V6H9.17L11.17 8H20V18ZM15 13H9V11H15V13ZM18 17H9V15H18V17Z" fill="currentColor" />
-    </svg>, label: 'Drafts'
+    icon: <img src="/assets_png/traits-svg/3-heads/head-mixer.svg" width="32" height="32" alt="Starred" />, 
+    label: 'Drafts'
   },
   {
-    icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.73 3H8.27L3 8.27V15.73L8.27 21H15.73L21 15.73V8.27L15.73 3ZM19 14.9L14.9 19H9.1L5 14.9V9.1L9.1 5H14.9L19 9.1V14.9Z" fill="currentColor" />
-      <path d="M12 17C12.5523 17 13 16.5523 13 16C13 15.4477 12.5523 15 12 15C11.4477 15 11 15.4477 11 16C11 16.5523 11.4477 17 12 17Z" fill="currentColor" />
-      <path d="M11 7H13V14H11V7Z" fill="currentColor" />
-    </svg>, label: 'Spam'
+    icon: <img src="/assets_png/traits-svg/3-heads/head-maze.svg" width="32" height="32" alt="Spam" />, 
+    label: 'Spam'
   },
   {
-    icon: <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M6 19C6 20.1 6.9 21 8 21H16C17.1 21 18 20.1 18 19V7H6V19ZM8 9H16V19H8V9ZM15.5 4L14.5 3H9.5L8.5 4H5V6H19V4H15.5Z" fill="currentColor" />
-    </svg>, label: 'Trash'
+  icon: <img src="/assets_png/traits-svg/3-heads/head-trashcan.svg" width="32" height="32" alt="Starred" />, 
+   label: 'Trash'
   },
 ]
 
@@ -83,11 +75,12 @@ export default function Home() {
         className="w-64 bg-[#121212] p-4 flex flex-col"
       >
         <div className="flex items-center mb-8">
-          <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
+          {/* <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
             <rect width="32" height="32" rx="4" fill="#D63C5E" />
             <path d="M8 8H24V24H8V8Z" fill="white" />
             <path d="M12 12H20V20H12V12Z" fill="#D63C5E" />
-          </svg>
+          </svg> */}
+          <img src="/assets_png/traits-svg/3-heads/head-crystalball.svg" width="50" height="50" alt="Mirage" />
           <h1 className="text-2xl font-bold">Mirage</h1>
         </div>
         <span className="text-sm text-gray-400">Decentralized Transactional Mail Service</span>
@@ -121,12 +114,10 @@ export default function Home() {
           className="bg-white text-[#121212] p-4 flex justify-between items-center"
         >
           <div className="flex space-x-4">
-            {/* <button
-              className={`px-4 py-2 rounded-full ${activeTab === 'Mail' ? 'bg-[#D63C5E] text-white' : 'bg-gray-200'}`}
-              onClick={() => setActiveTab('Mail')}
-            >
-              Mail
-            </button> */}
+            <button className="px-4 py-2 rounded-full flex items-center bg-gray-200">
+              <img src="/assets_png/traits-svg/3-heads/head-cordlessphone.svg" width="32" height="32" alt="Spam" style={{ transform: 'rotate(90deg)' }} className="mr-2" />
+              Contacts
+            </button>
             {/* <button
               className={`px-4 py-2 rounded-full ${activeTab === 'Subscription' ? 'bg-[#D63C5E] text-white' : 'bg-gray-200'}`}
               onClick={() => setActiveTab('Subscription')}
@@ -135,7 +126,8 @@ export default function Home() {
             </button> */}
           </div>
           <div className="flex items-center space-x-4">
-            <User size={24} />
+          {/* {randomImage && <img src={randomImage} alt="Random Profile" width={24} height={24} />} */}
+          <User size={24} />
             {/* <MessageSquare size={24} />
             <Bell size={24} />
             <Gift size={24} /> */}
@@ -155,7 +147,10 @@ export default function Home() {
                 transition={{ duration: 0.5 }}
                 className="bg-white rounded-lg p-6 shadow-lg"
               >
-                <h2 className="text-2xl font-bold mb-4 text-[#121212]">Compose Email</h2>
+                <h2 className="text-2xl font-bold mb-4 text-[#121212] flex items-center">
+                  <img src="/assets_png/traits-svg/3-heads/head-factory-dark.svg" width="60" height="60" alt="Compose" className="mr-2" />
+                  Compose Email
+                </h2>
                 <div className="flex items-center mb-4">
                   <input
                     type="text"
@@ -188,7 +183,8 @@ export default function Home() {
                 <div className="flex justify-between items-center">
                   <div className="flex space-x-2">
                     <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
-                      <Paperclip size={20} className="text-gray-500" />
+                      {/* <Paperclip size={20} className="text-gray-500" /> */}
+                      <img src="/assets_png/traits-svg/3-heads/head-paperclip.svg" width="50" height="50" alt="Mirage" />      
                     </motion.button>
                   </div>
                   <div className="flex space-x-2">
@@ -205,7 +201,7 @@ export default function Home() {
                       whileTap={{ scale: 0.95 }}
                       className="bg-[#D63C5E] text-white px-4 py-2 rounded flex items-center"
                     >
-                      <Send size={16} className="mr-2" />
+                      <img src="/assets_png/traits-svg/3-heads/head-helicopter.svg" width="40" height="40" alt="Mirage" />
                       Send
                     </motion.button>
                   </div>
